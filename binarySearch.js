88ecef9f766461c9ae48016302bd869c9d005adb
Amr Guaily <amrguaily@gmail.com>
2022-04-24 11:34:11 +0200

    binary Search

--- binarySearch.js
+++ binarySearch.js
@@ -26,5 +26,35 @@ function binarySearch(list, target) {
       start = mid + 1;
     }
   }
+  return null;
 }
-console.log(binarySearch([1, 3, 5, 7, 9], 7)); // output: 3 (the index of target elemrnt)
+binarySearch([1, 3, 5, 7, 9], 7); // output: 3 (the index of target elemrnt)
+
+/*
+problem: ## Search Insert Position - LeetCode ##
+Given a sorted array of integers and a target value, return the index if the target is found.
+If not, return the index where it would be if it were inserted in order.
+*/
+
+function searchInsert(nums, target) {
+  let start = 0,
+    end = nums.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2),
+      guess = nums[mid];
+
+    if (guess === target) {
+      return mid;
+    }
+
+    if (guess > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return start;
+}
+
+searchInsert([1, 3, 5, 6], 2);
